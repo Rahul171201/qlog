ef54371a8ef8dcd9105101a4b23d822bd6a39dba
Rahul171201 <rahulroydps@gmail.com>
2023-02-06 19:52:50 +0530

    updated question page with answer card components

---
+++ src/components/AnswerCard/AnswerCard.js
@@ -0,0 +1,33 @@
+import styles from "./AnswerCard.module.css";
+import Image from "next/image";
+
+const AnswerCard = () => {
+  return (
+    <div className={styles.answerWrapper}>
+      <div className={styles.leftBox}>
+        <div className={styles.upVote}>
+          <span className={styles.upvoteCount}>363</span>
+        </div>
+        <div className={styles.downVote}>
+          <span className={styles.downvoteCount}>15</span>
+        </div>
+      </div>
+      <div className={styles.rightBox}>
+        <div className={styles.header}>
+          <span>Rahul Roy</span>
+          <span>12 hours ago</span>
+        </div>
+        <hr className={styles.horizontalRule}></hr>
+        <div className={styles.content}>
+          Lorem ipsum hai bina detool ke paani jisse dhoya na jaa sake woh
+          banegi ek raani kuc bhi bolna ho toh bol do koi nahi soch rha bolte
+          waqt hum kisi bhi trake se nahi jaante tumhra appa kon hai mein bas
+          itna janat hun ki hum benasseb hein aur kuch nahijam jam jam chma cham
+          cham pum pum pum rol ol rol
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AnswerCard;
