7984c97af8fb15488690ec5415e21edd78174ff4
Rahul171201 <rahulroydps@gmail.com>
2023-02-07 18:05:20 +0530

    navbar design made

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,36 @@
 import Logo from "@/components/Logo/Logo";
 import Search from "@/components/Search/Search";
 import styles from "./Navbar.module.css";
+import { Lato } from "@next/font/google";
+import Image from "next/image";
+
+const lato = Lato({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
       <Logo></Logo>
+      <div className={`${styles.navListWrapper} ${lato.className}`}>
+        <div className={styles.navList}>
+          <ul className={styles.navList}>
+            <li className={styles.navItem}>FEED</li>
+            <li className={styles.navItem}>PROFILE</li>
+            <li className={styles.navListItem}>LOGOUT</li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.hamburgerWrapper}>
+        <Image
+          src="/hamburger.png"
+          alt="hamburger-icon"
+          width={50}
+          height={50}
+          className={styles.hamburgerIcon}
+        ></Image>
+      </div>
       <Search></Search>
     </div>
   );
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,20 +4,19 @@ import Image from "next/image";
 const Search = () => {
   return (
     <div className={styles.searchWrapper}>
-      <input
-        placeholder="Search for questions"
-        className={styles.searchField}
-      ></input>
-      <Image
-        src="/magnifier.png"
-        alt="search-icon"
-        width={30}
-        height={30}
-        className={styles.searchIcon}
-      ></Image>
-      {/* <img
-        
-      ></img> */}
+      <div className={styles.inputWrapper}>
+        <input
+          placeholder="Search for questions"
+          className={styles.searchField}
+        ></input>
+        <Image
+          src="/magnifier.png"
+          alt="search-icon"
+          width={30}
+          height={30}
+          className={styles.searchIcon}
+        ></Image>
+      </div>
     </div>
   );
 };
