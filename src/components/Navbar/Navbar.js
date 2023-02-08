c852c838cee7899a00e22580a8c6156e9ef4acce
Rahul171201 <rahulroydps@gmail.com>
2023-02-08 11:13:51 +0530

    hamburger made for navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import Search from "@/components/Search/Search";
 import styles from "./Navbar.module.css";
 import { Lato } from "@next/font/google";
 import Image from "next/image";
+import { useState } from "react";
 
 const lato = Lato({
   weight: "400",
@@ -10,28 +11,53 @@ const lato = Lato({
 });
 
 const Navbar = () => {
+  const [hamburger, setHamburger] = useState(false);
+
+  const handleHamburger = () => {
+    setHamburger(!hamburger);
+  };
+
   return (
-    <div className={styles.navbar}>
-      <Logo></Logo>
-      <div className={`${styles.navListWrapper} ${lato.className}`}>
-        <div className={styles.navList}>
-          <ul className={styles.navList}>
-            <li className={styles.navItem}>FEED</li>
-            <li className={styles.navItem}>PROFILE</li>
-            <li className={styles.navListItem}>LOGOUT</li>
-          </ul>
+    <div className={styles.navbarWrapper}>
+      {hamburger ? (
+        <div className={styles.overlay} onClick={handleHamburger}></div>
+      ) : (
+        <></>
+      )}
+      <div className={styles.navbar}>
+        <Logo></Logo>
+        <div className={`${styles.navListWrapper} ${lato.className}`}>
+          <div className={styles.navList}>
+            <ul className={styles.navList}>
+              <li className={styles.navListItem}>FEED</li>
+              <li className={styles.navListItem}>PROFILE</li>
+              <li className={styles.navListItem}>LOGOUT</li>
+            </ul>
+          </div>
         </div>
+        <div className={`${styles.hamburgerWrapper} ${lato.className}`}>
+          <Image
+            src="/hamburger.png"
+            alt="hamburger-icon"
+            width={50}
+            height={50}
+            className={styles.hamburgerIcon}
+            onClick={handleHamburger}
+          ></Image>
+          {hamburger ? (
+            <div className={styles.hamburgerMenu}>
+              <ul className={styles.menuList}>
+                <li className={styles.hamburgerItem}>FEED</li>
+                <li className={styles.hamburgerItem}>PROFILE</li>
+                <li className={styles.hamburgerItem}>LOGOUT</li>
+              </ul>
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+        <Search></Search>
       </div>
-      <div className={styles.hamburgerWrapper}>
-        <Image
-          src="/hamburger.png"
-          alt="hamburger-icon"
-          width={50}
-          height={50}
-          className={styles.hamburgerIcon}
-        ></Image>
-      </div>
-      <Search></Search>
     </div>
   );
 };
