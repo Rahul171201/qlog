74985c48c2d9e784fdf6ce54b4178b33a99a6c7d
Rahul171201 <rahulroydps@gmail.com>
2023-02-08 13:28:24 +0530

    made ask and answer pages

---
+++ src/pages/answer.js
@@ -0,0 +1,12 @@
+import styles from "../styles/Answer.module.css";
+import Navbar from "@/components/Navbar/Navbar";
+
+const Answer = () => {
+  return (
+    <div className={styles.answerWrapper}>
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default Answer;
---
+++ src/pages/ask.js
@@ -0,0 +1,12 @@
+import styles from "../styles/Ask.module.css";
+import Navbar from "@/components/Navbar/Navbar";
+
+const Ask = () => {
+  return (
+    <div className={styles.askWrapper}>
+      <Navbar></Navbar>
+    </div>
+  );
+};
+
+export default Ask;
