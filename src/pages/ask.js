f65fcb14437f6475575b43e7b6d4225ba9c5be7d
Rahul171201 <rahulroydps@gmail.com>
2023-02-10 17:19:55 +0530

    ask page updated

--- src/pages/ask.js
+++ src/pages/ask.js
@@ -1,10 +1,87 @@
 import styles from "../styles/Ask.module.css";
 import Navbar from "@/components/Navbar/Navbar";
 
+import { Lato } from "@next/font/google";
+const lato = Lato({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const removeTag = (e) => {
+  e.remove();
+};
+
+const addTag = (e) => {
+  e.preventDefault();
+  const parentElement = e.target.parentNode;
+  const newElement = document.createElement("div");
+  const inputElement = document.createElement("input");
+  const deleteButton = document.createElement("button");
+  inputElement.style.width = "160px";
+  inputElement.style.height = "40px";
+  inputElement.style.padding = "10px";
+  deleteButton.style.width = "30px";
+  deleteButton.style.height = "30px";
+  deleteButton.style.backgroundColor = "#B73E3E";
+  deleteButton.style.border = "none";
+  deleteButton.style.borderRadius = "50%";
+  deleteButton.style.color = "white";
+  deleteButton.style.cursor = "pointer";
+  deleteButton.style.fontSize = "1.5rem";
+  deleteButton.onmouseenter = (e) => {
+    e.target.style.backgroundColor = "#DD5353";
+  };
+  deleteButton.onmouseleave = (e) => {
+    e.target.style.backgroundColor = "#B73E3E";
+  };
+  deleteButton.onclick = (e) => {
+    removeTag(e.target.parentNode);
+  };
+  deleteButton.style.display = "flex";
+  deleteButton.style.justifyContent = "center";
+  deleteButton.style.alignItems = "center";
+  deleteButton.innerHTML = "-";
+  newElement.append(inputElement);
+  newElement.append(deleteButton);
+  newElement.style.display = "flex";
+  newElement.style.width = "200px";
+  newElement.style.justifyContent = "space-between";
+  newElement.style.alignItems = "center";
+  newElement.style.marginBottom = "20px";
+  parentElement.prepend(newElement);
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(e.target[0].value);
+};
+
 const Ask = () => {
   return (
     <div className={styles.askWrapper}>
       <Navbar></Navbar>
+      <div className={`${styles.formWrapper} ${lato.className}`}>
+        <form className={styles.questionForm} onSubmit={handleSubmit}>
+          <div className={styles.titleWrapper}>
+            <label className={styles.label}>Title</label>
+            <input className={styles.titleInput}></input>
+          </div>
+          <div className={styles.descriptionWrapper}>
+            <label className={styles.label}>Description</label>
+            <textarea className={styles.descriptionInput}></textarea>
+          </div>
+          <div className={styles.tagsWrapper}>
+            <button onClick={addTag} className={styles.addTagButton}>
+              Add Tag
+            </button>
+          </div>
+          <div className={styles.submitButtonWrapper}>
+            <button className={styles.submitButton} type="submit">
+              POST
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
