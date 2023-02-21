0d935424e0cff64db3d714959a1467e9f2d9be26
Rahul171201 <rahulroydps@gmail.com>
2023-02-21 12:21:48 +0530

    loading screen made

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,34 @@
 import { useEffect } from "react";
 import Router from "next/router";
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+
+import { Lato } from "@next/font/google";
+
+const lato = Lato({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 export default function Home() {
   useEffect(() => {
     Router.push("/login");
   }, []);
 
-  return <main>404 page not found</main>;
+  return (
+    <main className={styles.mainWrapper}>
+      <div className={`${styles.loadingWrapper} ${lato.className}`}>
+        <div className={styles.loadingText}>Loading</div>
+        <div className={styles.iconWrapper}>
+          <Image
+            src="/images/loading.png"
+            alt="loading icon"
+            width={100}
+            height={100}
+            className={styles.icon}
+          ></Image>
+        </div>
+      </div>
+    </main>
+  );
 }
